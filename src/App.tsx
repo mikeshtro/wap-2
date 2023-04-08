3ba39017042b808ed27302ad6613dd59fa69b45c
dejvones <d3jv.david@gmail.com>
2023-04-08 16:30:28 +0200

    added draggable

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Canvas } from './components/Canvas';
+import { Canvas, OperationType } from './components/Canvas';
 import { GraphicType } from './Models/IGraphic';
+import {GiBrickWall, GiArrowCursor, GiFinishLine} from 'react-icons/gi'
+import {FaRobot} from 'react-icons/fa'
 
 function App() {
     const [remove, setRemove] = useState<boolean>(false)
-    const [selected, setSelected] = useState<GraphicType>(0)
-    const types : string[] = ["Zeď", "Cíl", "Robot"]
+    const [selected, setSelected] = useState<OperationType>(0)
+    const types : string[] = ["Kurzor","Zeď", "Cíl", "Robot"]
 
     return (
       <div className="App">
@@ -17,7 +19,10 @@ function App() {
           {types.map((type, index) => 
             <button key={index} onClick={() => setSelected(index)}
             style={{padding: 20, width: 100, border: 0, backgroundColor: index == selected ? 'white' : 'grey'}}>
+              <div>
+              { index == 0 ? <GiArrowCursor size={30}/> : index == 1 ? <GiBrickWall size={30}/> : index == 2 ? <GiFinishLine size={30}/> : <FaRobot size={30}/>}
               <h3>{type}</h3>
+              </div>
             </button>
           )}
         </div>
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -3,28 +3,38 @@ import { GraphicType, IGraphic } from '../Models/IGraphic';
 
 interface props {
     remove : boolean,
-    selected: GraphicType
+    selected: OperationType,
+}
+
+interface position {
+    x: number,
+    y: number
+}
+
+export enum OperationType {
+    Cursor,
+    Wall,
+    Finish,
+    Robot
 }
 
 export const Canvas = ({remove, selected} : props) => {
     const [graphics, setGraphics] = useState<IGraphic[]>([])
+    const [cursor, setCursor] = useState("default")
+    const [startPosition, setStartPosition] = useState<position | null>(null)
+    const [selectedGraphic, setSelectedGraphic] = useState<IGraphic | null>(null)
 
     const canvas = React.useRef<HTMLCanvasElement | null>(null);  
-  
+     
     function onClick(e : React.MouseEvent){
         if (remove){
-            console.log(graphics)
-            console.log(e.clientX, e.clientY)
-            const clicked = graphics.find(g => 
-                (g.boundingRect.x1 <= e.clientX && e.clientX <= g.boundingRect.x2) && 
-                (g.boundingRect.y1 <= e.clientY && e.clientY <= g.boundingRect.y2))
-            console.log(clicked)
+            const clicked = detectGraphic(e.clientX, e.clientY)
             if (clicked){
                 graphics.splice(graphics.indexOf(clicked), 1)
                 removeGraphic(clicked)
             }
         }
-        else {
+        if (selected != 0) {
         const width : number = 20
         const height: number = 50
         const graphic : IGraphic = {
@@ -32,10 +42,16 @@ export const Canvas = ({remove, selected} : props) => {
             y: e.clientY, 
             width: width, 
             height: height, 
-            type: selected, 
+            type: selected - 1, 
             boundingRect : {x1: e.clientX, x2: e.clientX + width, y1: e.clientY, y2: e.clientY + height}}
         addGraphic(graphic)
         }       
+    }
+
+    function detectGraphic(x: number, y: number) : IGraphic | undefined {
+        return graphics.find(g => 
+            (g.boundingRect.x1 <= x && x <= g.boundingRect.x2) && 
+            (g.boundingRect.y1 <= y && y <= g.boundingRect.y2))
     }
 
     function removeGraphic(graphic : IGraphic){
@@ -68,10 +84,42 @@ export const Canvas = ({remove, selected} : props) => {
             context.fillRect(graphic.x, graphic.y, graphic.width, graphic.height)
         }
     }
+
+    function dragMouseDown(e : React.MouseEvent){
+        if(selected != OperationType.Cursor) return
+        const clicked = detectGraphic(e.clientX, e.clientY)
+        if (clicked) {
+            setStartPosition({x: e.clientX, y: e.clientY})
+            setSelectedGraphic(clicked)
+            setCursor("move")
+        }
+    }
+
+    function dragUpDown(e : React.MouseEvent){
+        console.log(e)
+        if (startPosition && selectedGraphic){
+            const newX = selectedGraphic.x + e.clientX - startPosition.x 
+            const newY = selectedGraphic.y + e.clientY - startPosition.y
+
+            //Remove at old position
+            graphics.splice(graphics.indexOf(selectedGraphic), 1)
+            removeGraphic(selectedGraphic)
+
+            //Add to new position
+            addGraphic({
+                ...selectedGraphic, 
+                x: newX, 
+                y: newY, 
+                boundingRect: {x1: newX, x2: newX + selectedGraphic.width, y1: newY, y2: newY + selectedGraphic.height}, })
+            setSelectedGraphic(null)
+            setStartPosition(null)
+        }
+        setCursor("default")
+    }
   
     return (
-        <canvas onClick={onClick}
-        ref={canvas} height={600} width={1200} style={{backgroundColor: 'white'}}
+        <canvas onClick={onClick} onMouseDown={dragMouseDown} onMouseUp={dragUpDown}
+        ref={canvas} height={600} width={1200} style={{backgroundColor: 'white', cursor: cursor}}
         />
     );
 };
